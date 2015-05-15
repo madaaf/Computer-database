1215117e67e41a4020a9afc0f1de6ee49edbf0a1
Mada Aflak <madaaf123@gmail.com>
2015-05-15 18:13:29 +0200

    without DAOExc

--- src/main/java/exception/DAOConfigurationException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class DAOConfigurationException extends RuntimeException {
-	private String message;
-
-	public DAOConfigurationException(String message) {
-		this.message = message;
-	}
-}
--- src/main/java/exception/DAOException.java
+++ src/main/java/exception/DAOException.java
@@ -5,10 +5,9 @@ public class DAOException extends RuntimeException {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private String message;
+	String message;
 
 	public DAOException(String message) {
 		this.message = message;
 	}
-
 }
